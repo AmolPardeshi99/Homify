b42c6ebd1bda3e00f2837afb390d1e4ec00e9516
AmolPardeshi99 <pardeshiamol772@gmail.com>
2021-10-31 13:58:31 +0530

    amol2

--- app/src/main/java/dev/dominators/homify/ui/MainActivity.kt
+++ app/src/main/java/dev/dominators/homify/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package dev.dominators.homify
+package dev.dominators.homify.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dev.dominators.homify.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
