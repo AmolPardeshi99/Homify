3fe96fa5ea2c7412554cfb44d25a47b39fc74bde
dell <abhimanyumishra130@gmail.com>
2021-10-31 14:30:34 +0530

    Introduction

---
+++ app/src/main/java/dev/dominators/homify/ui/splash_screen/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package dev.dominators.homify.ui.splash_screen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.dominators.homify.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
