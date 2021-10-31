b6d42706840ab1c2745cdcfacf4c68b29fbdbdf6
dell <abhimanyumishra130@gmail.com>
2021-10-31 17:07:06 +0530

    intro_part

--- app/src/main/java/dev/dominators/homify/ui/Introduction/IntroActivity.kt
+++ app/src/main/java/dev/dominators/homify/ui/Introduction/IntroActivity.kt
@@ -1,12 +1,26 @@
 package dev.dominators.homify.ui.Introduction
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import dev.dominators.homify.R
+import dev.dominators.homify.databinding.ActivityIntroBinding
+import dev.dominators.homify.ui.MainActivity
 
 class IntroActivity : AppCompatActivity() {
+
+    private lateinit var dataBinding:ActivityIntroBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+        dataBinding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(dataBinding.root)
+
+        dataBinding.viewPager.adapter = ViewPagerAdapter(supportFragmentManager,lifecycle)
+        dataBinding.dotsIndicator.setViewPager2(dataBinding.viewPager)
+
+        dataBinding.skipIntro.setOnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/dev/dominators/homify/ui/splash_screen/SplashScreenActivity.kt
+++ app/src/main/java/dev/dominators/homify/ui/splash_screen/SplashScreenActivity.kt
@@ -1,12 +1,31 @@
 package dev.dominators.homify.ui.splash_screen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import dev.dominators.homify.R
+import dev.dominators.homify.databinding.ActivitySplashScreenBinding
+import dev.dominators.homify.ui.Introduction.IntroActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var dataBinding: ActivitySplashScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        dataBinding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(dataBinding.root)
+
+        var flag = CoroutineScope(Dispatchers.Main).async {
+            delay(2000)
+
+            val intent = Intent(applicationContext, IntroActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
