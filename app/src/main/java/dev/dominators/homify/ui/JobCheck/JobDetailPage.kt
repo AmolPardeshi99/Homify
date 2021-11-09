407261e29a954afa391bfd974095497ef9f7b3c6
dell <abhimanyumishra130@gmail.com>
2021-11-09 13:52:50 +0530

    job description

--- app/src/main/java/dev/dominators/homify/ui/JobCheck/JobDetailPage.kt
+++ app/src/main/java/dev/dominators/homify/ui/JobCheck/JobDetailPage.kt
@@ -5,10 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import dev.dominators.homify.R
+import dev.dominators.homify.databinding.FragmentJobDetailPageBinding
 
 class JobDetailPage : Fragment(R.layout.fragment_job_detail_page) {
 
+    private lateinit var binding:FragmentJobDetailPageBinding
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        binding = FragmentJobDetailPageBinding.bind(view)
+
+        binding.btnContactLead.setOnClickListener {
+
+            val progressBar = ProgressBar(requireContext())
+
+        }
+
+    }
 }
