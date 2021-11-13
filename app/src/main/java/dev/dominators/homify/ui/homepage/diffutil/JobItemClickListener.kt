e3dba89e9e20ec8c5e2c425331e3e07fdf48ae52
dell <abhimanyumishra130@gmail.com>
2021-11-13 18:12:39 +0530

    job description

--- app/src/main/java/dev/dominators/homify/ui/homepage/diffutil/JobItemClickListener.kt
+++
@@ -1,8 +0,0 @@
-package dev.dominators.homify.ui.homepage.diffutil
-
-import dev.dominators.homify.datamodel.NewJobData
-
-interface JobItemClickListener {
-
-    fun jobItemClicked(newJobData: NewJobData)
-}
